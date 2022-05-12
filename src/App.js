0b6d30d116bf5e489dbd488971bf7d5f4098224a
nguyenpat <nguyenpatzz@gmail.com>
2022-05-12 20:09:44 +0700

    use useEffect when first render show list movie

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
 import './App.css';
+import SearchIcon from "./SearchIcon.svg";
+import Github from "./Github.svg";
 
 // key: 4159debf
 const API_URL = "http://www.omdbapi.com/?apikey=4159debf&";
@@ -12,13 +15,51 @@ function App() {
 
   const [movieName, setMovieName] = useState("");
 
-  const callAPI = async () => {
-    
+  const searchMovies = async (title) => {
+    const datas = await fetch(`${API_URL}s=${title}`);
+
+    const data = await datas.json();
+
+    console.log(data.Search);
+
+    setMovies(data.Search);
+    setMovieName("");
   }
 
+  useEffect(() => {
+    searchMovies("Iron man");
+  }, []);
+
   return (
-    <div className="App">
-      
+    <div className="containter">
+
+      <header className="header">
+
+        <div className="header-title">
+          <h1>Movies</h1>
+        </div>
+
+        <div className="header-searchbox">
+          <input className="search-input" value={movieName} onChange={(e) => setMovieName(e.target.value)} type="text" placeholder="Search for movies" />
+          <img src={SearchIcon} alt="" className="search-img" onClick={() => searchMovies(movieName)}/>
+        </div>   
+      </header>
+
+      <main className="movie-container">
+        {
+          movies?.length > 0 ? (
+            movies.map((movie, index) => <MovieCard movie={movie} key={index} />)) 
+          : <div>empty</div>
+        }
+      </main>
+
+      <footer className="footer">
+        <p>nguyenpat</p>
+        <a href="" className="footer-github">
+          <img src={Github} alt="" />
+        </a>
+      </footer>
+
     </div>
   );
 }
