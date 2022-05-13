dc3bf73877513ecb5319798bc9d947fbf7a05f90
nguyenpat <nguyenpatzz@gmail.com>
2022-05-13 15:15:22 +0700

    create YearSeletor component

--- src/YearSelector.jsx
+++ src/YearSelector.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 
-function YearSelector(props) {
+function YearSelector({movies, setYear, year}) {
 
 
-	// console.log(props.movie);
-	// console.log(movie);
 
 	return (
-		<option value={props.movie.Year}>{props.movie.Year}</option>
+		<>
+			{/*<label htmlFor="years">Years</label>*/}
+        	<select id="years" onChange={(e) => setYear(e.target.value)}>
+        		<optgroup label="Year">
+        		<option className="year-option" value="all" defaultValue>All</option>
+        	{
+        	  movies.map((movie, index) => <option className="year-option" key={index} value={movie.Year}>{movie.Year}</option>)
+        	}
+        		</optgroup>
+        	</select>
+			{/*<option value={movie.Year}>{movie.Year}</option>*/}
+		</>
 	);
 }
 
